a6fda2450744a60a154a08b2ce0d24aad4414342
mathaweee2006 <76671830+mathaweee2006@users.noreply.github.com>
2022-05-27 20:13:40 +0700

    Add files via upload

---
+++ setupenv.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#install python
+sudo apt update
+sudo apt-get install python-is-python3 -y
+#install lip
+pip install torch torchvision
+
+#install git
+sudo apt install git-all
+
+git clone
