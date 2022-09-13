4201b9509879c7dfde698a86fb4ba2fd38f5fd8a
mathaweee2006 <76671830+mathaweee2006@users.noreply.github.com>
2022-09-13 21:43:16 +0700

    Update batchResize.py

--- batchResize.py
+++ batchResize.py
@@ -7,5 +7,5 @@ f = r'/rama3'
 for file in os.listdir(f):
     f_img = f+"/"+file
     img = Image.open(f_img)
-    img = img.resize((2296,1724))
+    img = img.resize((2048,1024))
     img.save(f_img)
