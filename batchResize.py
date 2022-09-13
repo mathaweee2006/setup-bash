72c8a679c9394fd3dda7544536e7bce737e65ab8
mathaweee2006 <76671830+mathaweee2006@users.noreply.github.com>
2022-09-13 21:41:38 +0700

    add batch

---
+++ batchResize.py
@@ -0,0 +1,11 @@
+import PIL
+import os
+import os.path
+from PIL import Image
+
+f = r'/rama3'
+for file in os.listdir(f):
+    f_img = f+"/"+file
+    img = Image.open(f_img)
+    img = img.resize((2296,1724))
+    img.save(f_img)
